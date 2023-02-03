16e32ae12886c454ab3680b238916713031ed3af
1sosed9 <vladsosed63@gmail.com>
2023-02-03 11:35:34 +0200

    delete from return console.log()

--- src/app.js
+++ src/app.js
@@ -8,15 +8,17 @@ function Auto(brand, model, createdYear, vinCode) {
 let auto = new Auto("Audi", "A-4", 2023, "1234567891234567");
 
 Auto.prototype.log = function () {
-    return console.log(`Auto {
+    return `Auto {
         brand: ${this.brand}, 
         model: ${this.model},
         createdYear = ${this.createdYear}
-        }`);
+        }`;
 }
 
+let arr = [1, 2, 3, 4];
+
 Auto.prototype.checkVin = function () {
-   return console.log(this.vinCode.length === 16? true: false); 
+    return this.vinCode.length === 16 ? true : false;
 }
 
 function Auto_Fuel(brand, model, createdYear, vinCode, engineСapacity, consumptionLiters) {
@@ -26,19 +28,19 @@ function Auto_Fuel(brand, model, createdYear, vinCode, engineСapacity, consumpt
     this.vinCode = vinCode;
     this.engineСapacity = engineСapacity;
     this.consumptionLiters = consumptionLiters;
-} 
+}
 
 Auto_Fuel.prototype = Object.create(Auto.prototype);
 
 Auto_Fuel.prototype.showFuelConsumption = function () {
-    return console.log(`
+    return console.log`
     Auto_Fuel {
         engineСapacity: ${this.engineСapacity},
         consumptionLiters: ${this.consumptionLiters}
-    }`);
+    }`;
 }
 
-let bmwX_1 = new Auto_Fuel("BMW", "X-1", 2018, "234567891234567", "2.0", 10); 
+let bmwX_1 = new Auto_Fuel("BMW", "X-1", 2018, "234567891234567", "2.0", 10);
 
 function Auto_Electric(brand, model, createdYear, vinCode, batteryСapacity) {
     this.brand = brand;
@@ -50,12 +52,17 @@ function Auto_Electric(brand, model, createdYear, vinCode, batteryСapacity) {
 Auto_Electric.prototype = Object.create(Auto.prototype);
 
 Auto_Electric.prototype.showBatteryConfig = function () {
-    return console.log(this.batteryСapacity);
+    return this.batteryСapacity;
 }
 
 let nissanLeaf = new Auto_Electric("Nissan", "Leaf", 2017, "1234567891234568", 100);
 
-nissanLeaf.log();
-nissanLeaf.checkVin();
-nissanLeaf.showBatteryConfig();
+let infoAboutCar = nissanLeaf.log();
+console.log(infoAboutCar);
+
+let vinNissan = nissanLeaf.checkVin();
+console.log(vinNissan);
+
+let showBatteryConfig = nissanLeaf.showBatteryConfig();
+console.log(showBatteryConfig);
 
